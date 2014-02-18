50221bb096d907408f7d39ded5dbb939a1aaeb66
Debra Zapata <debra.zapata@gmail.com>
2014-02-18 11:03:57 -0700

    Adding event tracking

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
- $.backstretch("images/rocks.jpg");
+$.backstretch("images/rocks.jpg");
+
+
+
+$('#twitter').on('click', function() {
+  ga('send', 'event', 'twitter', 'click', 'social-buttons');
+});
+
+$('#pinterest').on('click', function() {
+  ga('send', 'event', 'pinterest', 'click', 'social-buttons');
+});
+
+$('#github').on('click', function() {
+  ga('send', 'event', 'github', 'click', 'social-buttons');
+});
+
+$('#linkedin').on('click', function() {
+  ga('send', 'event', 'linkedin', 'click', 'social-buttons');
+});
